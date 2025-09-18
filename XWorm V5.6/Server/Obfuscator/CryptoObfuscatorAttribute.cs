9d90b87060e18eb442c6adcbf6e25445a4dd7f4e
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:52:51 +0400

    CryptoObfuscatorAttribute.cs

---
+++ XWorm V5.6/Server/Obfuscator/CryptoObfuscatorAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+internal class CryptoObfuscator\u002EProtectedWithCryptoObfuscatorAttribute : Attribute
+{
+  [DllImport("kernel32.dll", CharSet = CharSet.Ansi)]
+  [MethodImpl(MethodImplOptions.ForwardRef)]
+  public static extern IntPtr GetProcAddress([In] IntPtr obj0, [In] string obj1);
+}
