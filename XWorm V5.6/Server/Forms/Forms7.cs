16e8fb8c3be4be57a8142edbacb58ec1e9fef8b6
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:52:01 +0400

    Forms7.cs

---
+++ XWorm V5.6/Server/Forms/Forms7.cs
@@ -0,0 +1,112 @@
+﻿using EA7FF2D0C489326F0D2AFFF69BA6F82C;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Configuration;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using V462540EVV6738320BEAVB181B7B6441;
+
+namespace N1C62C6F47393CBF2355AN5E34307DD0
+{
+  [GeneratedCode("MyTemplate", "11.0.0.0")]
+  [EditorBrowsable(EditorBrowsableState.Never)]
+  internal class B2673BC82C5B1B0F6BAC43DC322CA48B : WindowsFormsApplicationBase
+  {
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static byte[] get__7zD() => throw new Exception("ぜ");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static Bitmap get__Error() => throw new Exception("べ");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static Bitmap get__Stop() => throw new Exception("く");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static string get_AKey() => throw new Exception("ゐ");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static string get_Binder() => throw new Exception("ど");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static Bitmap get_CD() => throw new Exception("ず");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static byte[] get_Chat() => throw new Exception("し");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static string get_Fixer() => throw new Exception("お");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static Bitmap get_Folder() => throw new Exception("お");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static Bitmap get_HDD() => throw new Exception("ざ");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static string get_HTA() => throw new Exception("い");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static Bitmap get_Information() => throw new Exception("だ");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static byte[] get_Intro() => throw new Exception("㍐");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static Bitmap get_Network() => throw new Exception("ぶ");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static Bitmap get_Play() => throw new Exception("ろ");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static Bitmap get_Question() => throw new Exception("ゐ");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static string get_Stub() => throw new Exception("え");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static Bitmap get_USB() => throw new Exception("ず");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static string get_VBCode() => throw new Exception("ぬ");
+
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static Bitmap get_Warning() => throw new Exception("な");
+
+    static B2673BC82C5B1B0F6BAC43DC322CA48B()
+    {
+      \u003CModule\u003E.V9E7DEFV177BBC8C3067A0073440VDVB = (VEF87313E4DDFCV3A9EE7908EAV73C56) SettingsBase.Synchronized((SettingsBase) new VB78A3E6E8DF638C2DV113AV7CD8B3DD());
+      \u003CModule\u003E.E665199918FE8F7F9200DE2CB7A76DA0 = RuntimeHelpers.GetObjectValue(new object());
+    }
+
+    internal delegate int GetProcA([In] IntPtr obj0, [In] IntPtr obj1);
+
+    internal delegate int GetProcA2([In] B2673BC82C5B1B0F6BAC43DC322CA48B.WL obj0, [In] IntPtr obj1);
+
+    internal delegate void WL();
+
+    internal delegate void GetProcA3();
+  }
+}
