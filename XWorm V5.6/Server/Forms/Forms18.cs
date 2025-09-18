cd0767f9f6051b92a6bda524879289f08bc28ee7
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:09 +0400

    Forms18.cs

---
+++ XWorm V5.6/Server/Forms/Forms18.cs
@@ -0,0 +1,509 @@
+﻿using FastColoredTextBoxNS;
+using Guna.UI2.WinForms;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace DCF5626620B93114F5EA3D2F3037B9D5
+{
+  [DesignerGenerated]
+  public class D40EC4297AB137B17D850F2F00931D78 : Form
+  {
+    internal Form \u0024S0;
+    internal Exception \u0024VB\u0024ResumableLocal_ex\u00241;
+    internal Exception \u0024VB\u0024ResumableLocal_ex\u00242;
+    internal TaskAwaiter \u0024A0;
+    public ushort Reserved;
+    public ushort Type;
+    public ushort Count;
+    public byte Width;
+    public byte Height;
+    public byte ColorCount;
+    public byte Reserved;
+    public ushort Planes;
+    public ushort BitCount;
+    public int BytesInRes;
+    public int ImageOffset;
+    public uint Size;
+    public int Width;
+    public int Height;
+    public ushort Planes;
+    public ushort BitCount;
+    public uint Compression;
+    public uint SizeImage;
+    public int XPelsPerMeter;
+
+    internal virtual FastColoredTextBox editor
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("こ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ぁ");
+    }
+
+    internal virtual StatusStrip StatusStrip1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("い");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("り");
+    }
+
+    internal virtual ToolStripStatusLabel ToolStripStatusLabel1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ゎ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("じ");
+    }
+
+    internal virtual ListBox ListBox1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("き");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ヱ");
+    }
+
+    internal virtual Panel Panel1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("い");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("う");
+    }
+
+    internal virtual Timer Timer1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("や");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ぬ");
+    }
+
+    internal virtual Panel Panel5
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("げ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("め");
+    }
+
+    internal virtual Guna2Panel Guna2Panel1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("す");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ゖ");
+    }
+
+    internal virtual Guna2GradientButton Guna2GradientButton1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("は");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("か");
+    }
+
+    internal virtual Guna2Panel Guna2Panel3
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぴ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("う");
+    }
+
+    internal virtual Guna2Panel Guna2Panel2
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("げ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("び");
+    }
+
+    internal virtual Guna2ContextMenuStrip Guna2ContextMenuStrip2
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ゝ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("で");
+    }
+
+    internal virtual ToolStripMenuItem AddToolStripMenuItem1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぇ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("が");
+    }
+
+    internal virtual ToolStripMenuItem RemoveToolStripMenuItem1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("と");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ぞ");
+    }
+
+    internal virtual Label Label1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("さ");
+      [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("な");
+    }
+
+    internal virtual Guna2ContextMenuStrip Guna2ContextMenuStrip1
+    {
+      [MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("や");
+      [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("こ");
+    }
+
+    internal virtual ToolStripMenuItem CutToolStripMenuItem1
+    {
+      [MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぢ");
+      [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("や");
+    }
+
+    internal virtual ToolStripMenuItem CopyToolStripMenuItem1
+    {
+      [MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("の");
+      [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("え");
+    }
+
+    internal virtual ToolStripMenuItem PasteToolStripMenuItem1
+    {
+      [MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぐ");
+      [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("さ");
+    }
+
+    internal virtual ToolStripMenuItem ToolStripMenuItem7
+    {
+      [MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぎ");
+      [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ぜ");
+    }
+
+    internal virtual ToolStripMenuItem ErrorTestToolStripMenuItem
+    {
+      [MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("よ");
+      [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ポ");
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void DABAADF8BFD42D7A184EE7D63A516501(object sender, EventArgs e)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("そ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void D12A5FD91DF519FEB4A64AB17F4B6171(
+      CodeDomProvider codeDomProvider,
+      string source,
+      string[] referencedAssemblies)
+    {
+      try
+      {
+        try
+        {
+        }
+        finally
+        {
+label_3:
+          goto label_4;
+        }
+        goto label_3;
+      }
+      catch (Exception ex)
+      {
+label_4:
+        throw new Exception("ふ");
+      }
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void D68208A04E93604C91273D0899FDDEC0(object sender, EventArgs e) => throw new Exception("め");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void DFB61FD2118D298FC46EEAD5CB2DCB16(object sender, FormClosingEventArgs e) => throw new Exception("ぐ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void E80C901B5611BC3CACC7219F303C8E21(object sender, EventArgs e)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ゖ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void D7490DC8AF90C6BD0DB40587CD0F0F53(object sender, EventArgs e) => throw new Exception("そ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void B7BC275B518DDE4EBE3615AA81E585B5(object sender, EventArgs e)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("じ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void D61C5CB2DF9AC0F35D9C2EDC1E988CDE(object sender, EventArgs e)
+    {
+      try
+      {
+        try
+        {
+        }
+        finally
+        {
+label_3:
+          goto label_4;
+        }
+        goto label_3;
+      }
+      catch (Exception ex)
+      {
+label_4:
+        throw new Exception("ざ");
+      }
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void DFF13E9B8676CCEBDBDBCCC795B95CF6(object sender, EventArgs e)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("を");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void DC944FDED96E9511A365B2F911A3995A(object sender, EventArgs e)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ぎ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void A41E3BD67E3B0990E2DA6A33043ECBB9(object sender, EventArgs e)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ぶ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void DCEA1162A7F4D0801AB58B0CBE08F85F(object sender, EventArgs e) => throw new Exception("ほ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void DE9AD5B8C12AB0D157B35E84E20D2803(object sender, EventArgs e)
+    {
+      try
+      {
+      }
+      finally
+      {
+label_2:
+        throw new Exception("ほ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public D40EC4297AB137B17D850F2F00931D78() => throw new Exception("ひ");
+
+    [DebuggerNonUserCode]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    protected override void Dispose(bool disposing)
+    {
+      try
+      {
+      }
+      finally
+      {
+label_2:
+        throw new Exception("だ");
+      }
+      goto label_2;
+    }
+
+    [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void D4B96A9CF1E2DFD4C6C4A043AFCA1E76() => throw new Exception("う");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Timer get_Timer1() => throw new Exception("な");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Timer1(Timer WithEventsValue) => throw new Exception("い");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual StatusStrip get_StatusStrip1() => throw new Exception("す");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_StatusStrip1(StatusStrip WithEventsValue) => throw new Exception("ぢ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual ToolStripStatusLabel get_ToolStripStatusLabel4() => throw new Exception("ゆ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_ToolStripStatusLabel4(ToolStripStatusLabel WithEventsValue) => throw new Exception("な");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Panel get_Panel2() => throw new Exception("ぴ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Panel2(Panel WithEventsValue) => throw new Exception("ぁ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2ComboBox get_Guna2ComboBox2() => throw new Exception("に");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2ComboBox2(Guna2ComboBox WithEventsValue) => throw new Exception("ち");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2GradientButton get_Guna2GradientButton1() => throw new Exception("す");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2GradientButton1(Guna2GradientButton WithEventsValue) => throw new Exception("ぺ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2ComboBox get_Guna2ComboBox1() => throw new Exception("ほ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2ComboBox1(Guna2ComboBox WithEventsValue) => throw new Exception("ひ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual PictureBox get_PictureBox1() => throw new Exception("ゕ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_PictureBox1(PictureBox WithEventsValue) => throw new Exception("て");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2Panel get_Guna2Panel1() => throw new Exception("げ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2Panel1(Guna2Panel WithEventsValue) => throw new Exception("ち");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2Panel get_Guna2Panel3() => throw new Exception("ぃ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2Panel3(Guna2Panel WithEventsValue) => throw new Exception("り");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2Panel get_Guna2Panel2() => throw new Exception("べ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2Panel2(Guna2Panel WithEventsValue) => throw new Exception("い");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void DCA7DD4213EE6DFA55FF1D2EB7824A9F(object sender, EventArgs e)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ぶ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void DDDA95ABB681D569DF1BD8AAD9B92B12(MemoryStream b)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("こ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void D6A898BCCB978E95DDBDEEFC355CC58F(object sender, EventArgs e) => throw new Exception("ぴ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void D38999160F84E2D06BD915325A5848DB(object sender, FormClosingEventArgs e) => throw new Exception("ヸ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void D75AA0B35503E004B7AFFFE3AB6226FC(object sender, EventArgs e)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("じ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void E8FC4CBFBE2BA7C3958688CEB8B96BB4(object sender, EventArgs e) => throw new Exception("え");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void CC9CAA2615C73B75221AA9A8C077CFFD(object sender, EventArgs e) => throw new Exception("ヸ");
+  }
+}
