cc793c3d5a175839e4f2dc62d008209b191d7370
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:49:35 +0400

    09.cs

---
+++ XWorm V5.6/Server/09.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+internal class \u0028\u0029\u003B\u0009 : Attribute
+{
+  public static byte[] XFE40FXCE68B47540898XE5DA4F7XE0A([In] Stream obj0)
+  {
+    using (MemoryStream destination = new MemoryStream())
+    {
+      obj0.CopyTo((Stream) destination);
+      return destination.ToArray();
+    }
+  }
+}
