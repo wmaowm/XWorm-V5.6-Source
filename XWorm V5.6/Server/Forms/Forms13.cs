2e2044a15e6701e10c1c453b94098f9114086b40
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:49:59 +0400

    Forms13.cs

---
+++ XWorm V5.6/Server/Forms/Forms13.cs
@@ -0,0 +1,48 @@
+﻿using M5E7485F8B1A1AD7481M48DMCMC4A3DC;
+using M91M4ECFDDM3DME6A47137F5C799218A;
+using MEA9MCF25F24475D52DDBEE8F296A691;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace M8C21CMCB1FC7C995D9226A3AE1066C3
+{
+  public class C274715AC8761C8D85911DEC211DDC6E : ListView
+  {
+    internal TaskAwaiter \u0024A0;
+    public int \u0024State;
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sValue() => throw new Exception("お");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_sValue(string AutoPropertyValue) => throw new Exception("み");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public C274715AC8761C8D85911DEC211DDC6E() => throw new Exception("ま");
+
+    public class clsListviewSorter : IComparer
+    {
+      public static int D8084DBA5F18A3E169535B623B96B228;
+      public static int DAB747C1D73E30E0B97D6FEE7309DF49;
+
+      public clsListviewSorter(
+        D0390509B52C44661FC1BBD32C057C4B._Closure\u0024__R5\u002D8 arg0)
+      {
+        if (arg0 == null)
+          return;
+        ((F37068503FD78A2484752A672949A328) this).\u0024VB\u0024NonLocal_10 = ((F37068503FD78A2484752A672949A328) arg0).\u0024VB\u0024NonLocal_10;
+      }
+
+      [DebuggerHidden]
+      [SpecialName]
+      internal void _Lambda\u0024__R18(object a0) => ((M5D21ECED72ME2727DF3B0M1DC743CC6) ((F37068503FD78A2484752A672949A328) this).\u0024VB\u0024NonLocal_10).A0971C0149162C7886175E2F76907783((byte[]) a0);
+    }
+  }
+}
