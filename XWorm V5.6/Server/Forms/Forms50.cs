c564c44e9a2276b259be44684d978ccb880dd823
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:21 +0400

    Forms50.cs

---
+++ XWorm V5.6/Server/Forms/Forms50.cs
@@ -0,0 +1,49 @@
+﻿using IF7ECB736801D875D48E6C65CE1BIEF6;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace M97E66DDC5D6FEDC246A7B4559E7CC3C
+{
+  public class M9624D6AM60E68CC779BM8C4B6M8FE24 : I2F88ED16A41I5FEI8E9B06BFCAF5E96
+  {
+    public static int B7BB7060348867882F4E5D81AE9AF2B0;
+    public static int LBL1BF038121F592LL7BF5D5035C1A6C;
+
+    public string sName
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぱ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("け");
+    }
+
+    public string sValue
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("お");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("み");
+    }
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_name() => throw new Exception("さ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_name(string AutoPropertyValue) => throw new Exception("ぴ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_path() => throw new Exception("け");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_path(string AutoPropertyValue) => throw new Exception("る");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_sameSite() => throw new Exception("そ");
+  }
+}
