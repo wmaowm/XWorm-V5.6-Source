8486becbb392f319da99d39a9c70d9ec5c9b576d
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:16 +0400

    Forms20.cs

---
+++ XWorm V5.6/Server/Forms/Forms20.cs
@@ -0,0 +1,281 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace F9487B3059A02C7F6F52542FEB8E1554
+{
+  [StandardModule]
+  [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+  [DebuggerNonUserCode]
+  [CompilerGenerated]
+  [HideModuleName]
+  internal sealed class D82F2E848FD75538009F57FABA58D5FE
+  {
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static ResourceManager ResourceManager
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("に");
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static CultureInfo Culture
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぜ");
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("げ");
+    }
+
+    internal static byte[] _7z
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ゆ");
+    }
+
+    internal static byte[] _7zD
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぜ");
+    }
+
+    internal static Bitmap _Error
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("べ");
+    }
+
+    internal static Bitmap _Stop
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("く");
+    }
+
+    internal static string AKey
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ゐ");
+    }
+
+    internal static string Binder
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ど");
+    }
+
+    internal static Bitmap CD
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ず");
+    }
+
+    internal static byte[] Chat
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("し");
+    }
+
+    internal static string Fixer
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("お");
+    }
+
+    internal static Bitmap Folder
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("お");
+    }
+
+    internal static Bitmap HDD
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ざ");
+    }
+
+    internal static string HTA
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("い");
+    }
+
+    internal static Bitmap Information
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("だ");
+    }
+
+    internal static byte[] Intro
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("㍐");
+    }
+
+    internal static Bitmap Network
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぶ");
+    }
+
+    internal static Bitmap Play
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ろ");
+    }
+
+    internal static Bitmap Question
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ゐ");
+    }
+
+    internal static string Stub
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("え");
+    }
+
+    internal static Bitmap USB
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ず");
+    }
+
+    internal static string VBCode
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぬ");
+    }
+
+    internal static Bitmap Warning
+    {
+      [SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("な");
+    }
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_LinkLabel4(LinkLabel WithEventsValue) => throw new Exception("ゔ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Label get_Label7() => throw new Exception("を");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Label7(Label WithEventsValue) => throw new Exception("の");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Label get_Label1() => throw new Exception("ひ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Label1(Label WithEventsValue) => throw new Exception("ぅ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual LinkLabel get_LinkLabel2() => throw new Exception("え");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_LinkLabel2(LinkLabel WithEventsValue) => throw new Exception("ろ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Label get_Label3() => throw new Exception("び");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Label3(Label WithEventsValue) => throw new Exception("べ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Label get_Label4() => throw new Exception("わ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Label4(Label WithEventsValue) => throw new Exception("れ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual Label get_Label2() => throw new Exception("む");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_Label2(Label WithEventsValue) => throw new Exception("な");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual PictureBox get_PictureBox3() => throw new Exception("ょ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_PictureBox3(PictureBox WithEventsValue) => throw new Exception("ぎ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal virtual PictureBox get_PictureBox2() => throw new Exception("っ");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.Synchronized | MethodImplOptions.NoInlining)]
+    internal virtual void set_PictureBox2(PictureBox WithEventsValue) => throw new Exception("さ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public D82F2E848FD75538009F57FABA58D5FE() => throw new Exception("れ");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void C29D81569D422B1CA42BEBDE5AAD5CCE(object sender, EventArgs e) => throw new Exception("え");
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void B092FBE632AFA153388BFADA6056F262(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("じ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void EC2A18AA9F230ADF9E8D4E8E0DC5AFC0(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+      try
+      {
+      }
+      catch (Exception ex)
+      {
+label_2:
+        throw new Exception("ぁ");
+      }
+      goto label_2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public D82F2E848FD75538009F57FABA58D5FE() => throw new Exception("ず");
+
+    [DebuggerNonUserCode]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    protected virtual void Dispose(bool disposing)
+    {
+      try
+      {
+      }
+      finally
+      {
+label_2:
+        throw new Exception("け");
+      }
+      goto label_2;
+    }
+
+    [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void BBACA4869BB0820F26B0E4C088921BDA() => throw new Exception("と");
+  }
+}
