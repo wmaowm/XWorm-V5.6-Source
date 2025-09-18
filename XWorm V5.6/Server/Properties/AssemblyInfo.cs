2a0393e53fbd7cab5baa70f48152db410f4d8488
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:53:00 +0400

    AssemblyInfo.cs

---
+++ XWorm V5.6/Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XWorm")]
+[assembly: AssemblyDescription("XCoder")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XWorm")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("da0e7b82-9a56-4026-af48-52629b0d254b")]
+[assembly: AssemblyFileVersion("5.2.0.0")]
+[assembly: AssemblyVersion("5.2.0.0")]
